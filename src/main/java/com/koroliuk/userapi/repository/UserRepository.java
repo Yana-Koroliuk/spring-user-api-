fae04b013a984a30b7e4d054ed9e10ef728f54f9
Yana-Koroliuk <yana.koroliuk1@gmail.com>
2024-04-27 16:38:42 +0300

    Add method to find users by birth date range

--- src/main/java/com/koroliuk/userapi/repository/UserRepository.java
+++ src/main/java/com/koroliuk/userapi/repository/UserRepository.java
@@ -4,8 +4,11 @@ import com.koroliuk.userapi.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    List<User> findByBirthDateBetween(LocalDate start, LocalDate end);
 }
