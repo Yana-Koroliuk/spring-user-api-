7a18a724552ce5671eb0714a6dca3cef76eb30ac
Yana-Koroliuk <yana.koroliuk1@gmail.com>
2024-04-28 02:18:02 +0300

    Update exception handling

--- src/main/java/com/koroliuk/userapi/exceptionHandler/RestResponseExceptionHandler.java
+++ src/main/java/com/koroliuk/userapi/exceptionHandler/RestResponseExceptionHandler.java
@@ -3,32 +3,27 @@ package com.koroliuk.userapi.exceptionHandler;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
 import java.util.Map;
 
-@RestControllerAdvice
+@ControllerAdvice
 public class RestResponseExceptionHandler {
 
     @ExceptionHandler(value = IllegalArgumentException.class)
     public ResponseEntity<Map<String, Object>> handleIllegalArgument(IllegalArgumentException ex) {
-        Map<String, Object> body = Map.of(
-                "reason", "There is no accepted data",
-                "message", ex.getMessage()
-        );
+        Map<String, Object> body = createErrorResponse("There is no accepted data", ex.getMessage());
         return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(value = EntityNotFoundException.class)
     public ResponseEntity<Map<String, Object>> handleEntityNotFound(EntityNotFoundException ex) {
-        Map<String, Object> body = Map.of(
-                "reason", "There is no entity with such id",
-                "message", ex.getMessage()
-        );
+        Map<String, Object> body = createErrorResponse("There is no entity with such id", ex.getMessage());
         return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
     }
 
@@ -38,7 +33,6 @@ public class RestResponseExceptionHandler {
                 .stream()
                 .map(FieldError::getDefaultMessage)
                 .toList();
-
         Map<String, Object> body = Map.of(
                 "reason", "Input data is not valid",
                 "message", errors
@@ -46,12 +40,22 @@ public class RestResponseExceptionHandler {
         return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(value = HttpMessageNotReadableException.class)
+    public ResponseEntity<Map<String, Object>> handleHttpMessageNotReadable(HttpMessageNotReadableException ex) {
+        Map<String, Object> body = createErrorResponse("Input data is not valid", ex.getMessage());
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Map<String, Object>> handleGeneral(Exception ex) {
-        Map<String, Object> body = Map.of(
-                "reason", "Error occurred",
-                "message", ex.getMessage()
+        Map<String, Object> body = createErrorResponse("Error occurred", ex.getMessage());
+        return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private static Map<String, Object> createErrorResponse(String reason, String message) {
+        return Map.of(
+                "reason", reason,
+                "message", message
         );
-        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
 }
