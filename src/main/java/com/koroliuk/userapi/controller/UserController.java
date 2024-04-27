2e6630b95b2601ee93133c92e3db15b641ad62f9
Yana-Koroliuk <yana.koroliuk1@gmail.com>
2024-04-27 16:42:49 +0300

    Add endpoints for user update, delete  and date range search

--- src/main/java/com/koroliuk/userapi/controller/UserController.java
+++ src/main/java/com/koroliuk/userapi/controller/UserController.java
@@ -3,12 +3,16 @@ package com.koroliuk.userapi.controller;
 import com.koroliuk.userapi.dto.UserDTO;
 import com.koroliuk.userapi.model.User;
 import com.koroliuk.userapi.service.UserServiceImpl;
+import com.koroliuk.userapi.validation.ValidationGroups.*;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -25,5 +29,25 @@ public class UserController {
     public ResponseEntity<User> createUser(@Valid @RequestBody UserDTO userDto) {
         return new ResponseEntity<>(userService.createUser(userDto), HttpStatus.CREATED);
 
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @Valid @RequestBody UserDTO userDto) {
+        User updatedUser = userService.updateUser(id, userDto);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<User>> findByBirthDateRange(
+            @RequestParam("start") LocalDate start,
+            @RequestParam("end") LocalDate end) {
+        List<User> users = userService.findByBirthDateRange(start, end);
+        return ResponseEntity.ok(users);
     }
 }
